1844c83735c882063ef8a397d90c78f3c59341c1
Wisnu <wisnudewa2000@gmail.com>
2023-12-14 20:32:35 +0700

    upload tugas

---
+++ variable.js
@@ -0,0 +1,17 @@
+// const name = "wisnu";
+// // name = "eduwork";
+// console.log(name);
+
+// const student = {
+//     name: "Wisnu",
+//     class: "8"
+// }
+// student.faculty = "ilmu komputer";
+// console.log(student);
+
+let x = "10" + 10 + 10;
+let x1 = Number("10") + 10 + 10;
+let y = 10 + 10 + "10";
+let y1 = 10 + 10 + Number("10");
+let z = x1 + y1;
+console.log(z);
